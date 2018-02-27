77d184c81c53fb6036153ecdcb4b2146345d0169
Stephen Fegan <sfegan@gmail.com>
2018-02-27 15:37:31 +0100

    Remove Latex commands

--- README.ml
+++ README.ml
@@ -4,7 +4,7 @@ A very high energy gamma-ray survey of unidentified EGRET sources
 ### _Stephen Fegan, 2004_ ###
 
 A Dissertation Submitted to the Faculty of the DEPARTMENT OF PHYSICS
-In Partial Fulfillment of the Requirements For the Degree of
+In Partial Fulfilment of the Requirements For the Degree of
 DOCTOR OF PHILOSOPHY
 In the Graduate College
 THE UNIVERSITY OF ARIZONA,
@@ -12,9 +12,9 @@ THE UNIVERSITY OF ARIZONA,
 
 ---
 
-A survey of unidentified 100\,MeV \Gray sources is undertaken, with
-the Whipple 10\,m telescope, with the objective of detecting very high
-energy ($>350$\,GeV) \Gray emission. The survey consists of nineteen
+A survey of unidentified 100 MeV gamma-ray sources is undertaken, with
+the Whipple 10m telescope, with the objective of detecting very high
+energy (>350 GeV) gamma-ray emission. The survey consists of nineteen
 sets of observations of sources detected by the EGRET instrument on
 the Compton Gamma-Ray Observatory between 1991 and 1995. Results for
 21 EGRET sources are reported; in some cases two EGRET sources are
@@ -23,8 +23,8 @@ source, candidate associations are listed and the implications of each
 candidate for VHE emission discussed. Finally, a study of the
 performance of a next-generation ground based instrument, VERITAS,
 using simulations is presented. The implications of the increased
-sensitivity of such an instrument for suture \Gray surveys is briefly
-discussed.
+sensitivity of such an instrument for suture gamma-ray surveys is
+briefly discussed.
 
 ---
 
